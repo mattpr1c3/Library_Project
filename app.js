9f71c1240598ed2f125622f5f6be283c537272e7
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-03-21 14:37:19 -0700

    started project

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-console.log("Hello World!\n==========\n");
+ class Book {
+    constructor(id, title, author, read) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.read = read;
+    }
+}
 
-// PROJECT Section
-console.log("PROJECT:\n==========\n");
+class Library {
+    constructor(bookCount, books) {
+        this.bookCount = bookCount;
+        this.books = books;
+    }
+    markRead(checkbox, id) {
+        for (let book = 0; book <= library.books; book++) {
+            if (book.id == checkbox.id) {
+                console.log(checkbox.id) // check for remaining todo's and possible renaming of parameters
+            }
+        }
+    }
+    addBook() {
+        // dom manipulation
+        const newbook = new Book() // parameters will come from dom manipulation
+
+
+
+        this.bookCount++
+    }
+}
+
+const library = new Library(1, [{id:1, title:"Of Mice and Men", author:"John Steinback", read:true}]);
