7c653c32d06ae6e6aa05b6deafee313be52637ee
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-03-26 00:34:06 -0700

    completed project

--- app.js
+++ app.js
@@ -1,32 +1,102 @@
- class Book {
-    constructor(id, title, author, read) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.read = read;
-    }
+const books = [
+  {
+    id: 1,
+    title: "Name of the Wind",
+    author: "Patrick Rothfuss",
+    read: true,
+  },
+];
+
+class Book {
+  constructor(id, title, author, read) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.read = read;
+  }
 }
 
 class Library {
-    constructor(bookCount, books) {
-        this.bookCount = bookCount;
-        this.books = books;
+  constructor(books) {
+    this.bookCount = books.length;
+    this.books = books;
+  }
+  addBook(book) {
+    if (!book) {
+      // Select the inputs from the form -- title, author, and read
+      var title = document.getElementById("title");
+      var author = document.getElementById("author");
+      var read = document.getElementById("read");
+      // Increment book count property
+      this.nextId++;
+      // Create an instance from my Book class with the input values
+      var newBook = new Book(
+        this.nextId,
+        title.value,
+        author.value,
+        read.checked
+      );
+      // Push the new book instance into the books array
+      this.books.push(newBook);
     }
-    markRead(checkbox, id) {
-        for (let book = 0; book <= library.books; book++) {
-            if (book.id == checkbox.id) {
-                console.log(checkbox.id) // check for remaining todo's and possible renaming of parameters
-            }
-        }
-    }
-    addBook() {
-        // dom manipulation
-        const newbook = new Book() // parameters will come from dom manipulation
-
-
-
-        this.bookCount++
-    }
+    // Select the table body
+    const tbody = document.getElementById("tableBody");
+    // Create new table row
+    const newTr = document.createElement("tr");
+    newTr.classList.add(book ? book.id : newBook.id);
+    newTr.addEventListener("dblclick", () => {
+      this.removeBook(book ? book.id : newBook.id);
+    });
+    // Create three new table data cells
+    const newTitle = document.createElement("td");
+    const newAuthor = document.createElement("td");
+    const newRead = document.createElement("td");
+    // Add text content to td's with book values
+    newTitle.textContent = book ? book.title : newBook.title;
+    newAuthor.textContent = book ? book.author : newBook.author;
+    const newCheckbox = document.createElement("input");
+    newCheckbox.classList.add(book ? book.id : newBook.id);
+    newCheckbox.type = "checkbox";
+    newCheckbox.checked = book ? book.read : read.checked;
+    newCheckbox.disabled = book ? book.read : read.checked;
+    newCheckbox.addEventListener("click", (event) => {
+      this.markRead(event.target, book ? book.id : newBook.id);
+    });
+    newRead.appendChild(newCheckbox);
+    // Append the td's to the tr
+    newTr.appendChild(newTitle);
+    newTr.appendChild(newAuthor);
+    newTr.appendChild(newRead);
+    // Append the tr to the body
+    tbody.appendChild(newTr);
+  }
+  markRead(checkbox, id) {
+    this.books.forEach((book) => {
+      if (id === book.id) {
+        book.read = true;
+        checkbox.disabled = true;
+      }
+    });
+  }
+  removeBook(bookId) {
+    // Reassign the books array after filtering out the remove book
+    this.books = this.books.filter(({ id }) => bookId !== id);
+    // Remove the book from the DOM
+    const tbody = document.getElementById("tableBody");
+    tbody.removeChild(document.getElementsByClassName(bookId)[0]);
+  }
 }
 
-const library = new Library(1, [{id:1, title:"Of Mice and Men", author:"John Steinback", read:true}]);
+const library = new Library(books);
+if (books.length > 0) {
+  library.books.forEach((book) => {
+    library.addBook(book);
+  });
+}
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  library.addBook();
+});
